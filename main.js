e70b54fdbf335245f51fac633529cabef5835980
elquees <erickkiehnle@gmail.com>
2023-03-25 13:00:17 -0600

    inicio de conexion mysql

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow, ipcMain} = require('electron');
 const path = require('path');
+const mysql = require('mysql2');
 
 let ventana;
 
@@ -15,3 +16,26 @@ function createWindow() {
 }
 
 app.whenReady().then(createWindow)
+
+//crear conexion
+const conexion = mysql.createConnection({
+    host: 'localhost',
+    user: 'usuario1',
+    password: 'password',
+    database: 'consultas_actividad5'
+});
+
+/* conexion.query(
+    'SELECT * FROM consultas',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+    }
+  ); */
+
+conexion.promise().query('SELECT * FROM consultas')
+    .then(
+        ([result, fields]) => {
+            console.log(result)
+        }
+    )
+    .catch(err => console.log(err));
